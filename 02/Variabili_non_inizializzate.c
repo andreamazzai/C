9d292fbca9457b09a9e2088e354ca2a288568965
Andrea Mazzai <andreamazzai@gmail.com>
2023-12-05 00:05:04 +0100

    Altre prove

---
+++ 02/Variabili_non_inizializzate.c
@@ -0,0 +1,14 @@
+/* Esercizio variabili non inizializzate */
+#include <stdio.h>
+
+int main(void) {
+    int prima_int, seconda_int;
+    float prima_flo, seconda_flo;
+
+    printf("valore di   prima_int: %d\n", prima_int);
+    printf("valore di seconda_int: %d\n", seconda_int);
+    printf("valore di   prima_flo: %f\n", prima_flo);
+    printf("valore di secondaflot: %f\n", seconda_flo);
+    printf("valore di   prima_flo: %g\n", prima_flo);
+    printf("valore di seconda_flo: %g\n", seconda_flo);
+}
